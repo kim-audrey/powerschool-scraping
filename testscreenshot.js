44a6f1a31b2cd8c11eb95ec1638e487d554657e6
Alex <adruzenko03@gmail.com>
2020-06-01 11:55:14 -0400

    undo last

--- testscreenshot.js
+++ testscreenshot.js
@@ -1,14 +1,11 @@
 const puppeteer = require('puppeteer');
 
-
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://ps001.bergen.org/public/home.html');
-  await page.type('#username', 'username');
-  await page.type('#password', 'password');
+  
   //we can change as need be
-  // screenshot area
   await page.setViewport({
     width: 800,
     height: 600,
