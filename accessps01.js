f2e75628f7aec9d01a43ea602782e54f8c23ee2d
ajk8703 <ajkim8703@gmail.com>
2020-06-02 19:11:01 -0400

    Fingers Crossed

--- accessps01.js
+++ accessps01.js
@@ -5,26 +5,22 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
   await page.goto('https://ps001.bergen.org/public/home.html');
   
-  //we can change as need be
-  await page.setViewport({
-    width: 800,
-    height: 600,
-    deviceScaleFactor: 1,
-  }); await page.screenshot({path: 'prelogin.png'});
 
   /* LOGGING IN */
-  await page.type('#fieldAccount', "a@bergen.org", {     delay:30    })
-  await page.type('#fieldPassword', "stuff", {    delay: 30   })
+  await page.type('#fieldAccount', "audkim21", {     delay:30    })
+  await page.type('#fieldPassword', "29137005095432a*", {    delay: 30   })
 
 
   // click sign in button
   await page.click('#btn-enter');
   
+
   await page.setViewport({
-    width: 800,
-    height: 600,
+    width: 1350,
+    height: 625,
     deviceScaleFactor: 1,
   });  await page.screenshot({path: 'loggingin.png'});
+
 
   await page.waitForNavigation({  waitUnitl: 'networkidle0    '});
   await page.waitFor(1500000);
