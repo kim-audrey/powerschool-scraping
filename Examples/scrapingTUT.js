85cbc431a6b21fdc3ad5749b991dba53fc14837a
ajk8703 <ajkim8703@gmail.com>
2020-06-04 21:43:18 -0400

    added scrapingTUT

---
+++ Examples/scrapingTUT.js
@@ -0,0 +1,18 @@
+// before everything, I ran npm init -y which is supposed to create an empty package json
+
+const puppeteer = require('puppeteer');
+
+async function scrapeProduct(URL HERE){
+    const browers = awaitpuppeteer.launch();
+    const page = await browswer.newPage();
+    await page.goto(URL HERE);
+
+    // .$x is pupetteer's selector, select an item by xpath
+    const [el] = await page.$x('//*[@id="ccid_817487"]/td[13]/a');
+    const src = await el.getProperty('src');
+    const srcTxt = src.jsonValue();
+
+    console.log({srcTxt}); 
+
+
+}
